cb1ae36ffeb5a88393a1b58534ada6ec545406dc
ben.d.maryland@gmail.com <bend@RoseFan.localdomain>
2021-11-04 09:10:20 -0400

    commit tree 0ad54564e46aa5abe02e5636828c8a2f62bf89a8

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot (value="Squawk!")
+if value=="Squawk!"
+puts  "Squawk!"
+else
+puts "Pretty bird!"
+end
+
+
+
+value
+end 
